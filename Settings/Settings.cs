5ba5e366a99e0106321999c8e44ffc7f028836c8
Jordan Wood <jordanwood@Jordans-MacBook-Pro-2.local>
2019-01-02 22:17:49 -0700

    Initial Commit  - Project Import  - .Net Core 2.1 set up

---
+++ Settings/Settings.cs
@@ -0,0 +1,12 @@
+public static class GlobalSettings
+{
+    public static class CompanySettings
+    {
+        public static string CompanyName = "Consulting TBD";
+    }
+
+    public static class Cookies 
+    {
+
+    }
+} 
